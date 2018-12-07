e889d9c9b6c40e83bb58a0bf9dcf3f5ad554c037
Chris-McGraw <34453103+Chris-McGraw@users.noreply.github.com>
2018-12-07 10:51:13 -0500

    script-update-0

---
+++ recipeListMaster.js
@@ -0,0 +1,23 @@
+var chili = {
+  name: "Chili Con Carne",
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544118007/recipe-book/chili-con-carne.jpg"
+};
+
+var fajitas = {
+  name: "Fajitas",
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544117892/recipe-book/fajitas.jpg"
+};
+
+var spaghetti = {
+  name: "Spaghetti",
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544118748/recipe-book/spaghetti.jpg"
+};
+
+var springSalad = {
+  name: "Spring Salad",
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544121716/recipe-book/spring-salad.jpg"
+};
+
+
+
+var recipeListMaster = [chili, fajitas, spaghetti, springSalad];
