9562181c3ec36de9eff35450188a3920bd4243d4
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-12-22 16:43:15 -0500

    index-update-14 / style-update-23 / script-update-19

--- recipeListMaster.js
+++ recipeListMaster.js
@@ -1,56 +1,67 @@
 var bbqChicken = {
   name: "BBQ Chicken",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544379016/recipe-book/bbq-chicken.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544379016/recipe-book/bbq-chicken.jpg",
+  link: "recipes/template.html"
 };
 
 var chili = {
   name: "Chili Con Carne",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544118007/recipe-book/chili-con-carne.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544118007/recipe-book/chili-con-carne.jpg",
+  link: "recipes/template.html"
 };
 
 var fajitas = {
   name: "Fajitas",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544117892/recipe-book/fajitas.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544117892/recipe-book/fajitas.jpg",
+  link: "recipes/template.html"
 };
 
 var frittata = {
   name: "Frittata",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544402454/recipe-book/frittata.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544402454/recipe-book/frittata.jpg",
+  link: "recipes/template.html"
 };
 
 var hamburgers = {
   name: "Hamburgers",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544379558/recipe-book/hamburgers.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544379558/recipe-book/hamburgers.jpg",
+  link: "recipes/template.html"
 };
 
 var macAndCheese = {
   name: "Mac & Cheese",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544747322/recipe-book/mac-and-cheese.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544747322/recipe-book/mac-and-cheese.jpg",
+  link: "recipes/template.html"
 };
 
 var potatoCasserole = {
   name: "Potato Casserole",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544379270/recipe-book/potato-casserole.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544379270/recipe-book/potato-casserole.jpg",
+  link: "recipes/template.html"
 };
 
 var pulledPork = {
   name: "Pulled Pork",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544747322/recipe-book/pulled-pork.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544747322/recipe-book/pulled-pork.jpg",
+  link: "recipes/template.html"
 };
 
 var spaghetti = {
   name: "Spaghetti",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544118748/recipe-book/spaghetti.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544118748/recipe-book/spaghetti.jpg",
+  link: "recipes/template.html"
 };
 
 var springSalad = {
   name: "Spring Salad",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544121716/recipe-book/spring-salad.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544121716/recipe-book/spring-salad.jpg",
+  link: "recipes/template.html"
 };
 
 var tortelliniSoup = {
   name: "Tortellini Soup",
-  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544380718/recipe-book/tortellini-soup.jpg"
+  img: "https://res.cloudinary.com/dtwyohvli/image/upload/v1544380718/recipe-book/tortellini-soup.jpg",
+  link: "recipes/template.html"
 };
 
 
