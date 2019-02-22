97d04de27d21b631abc229c572347e6643040bdd
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-02-22 12:41:36 -0500

    Revert "SPA-multi-screen-update-70"
    This reverts commit 6f943d74d334faa923912e2d0353300afebe7663.

--- new-recipe-finder-screen-script.js
+++ new-recipe-finder-screen-script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   function newRecipeFinderSearchFocused() {
     window.scrollTo(0, 0);
 
-    /* $(document.body).css("overflow", "hidden"); */
+    $(document.body).css("overflow", "hidden");
 
     $navbar.hide();
     $navbarDropdown.hide();
@@ -17,12 +17,12 @@ $(document).ready(function() {
       $bodyGridContainer.css("top", "20px");
     }
 
-    $newRecipeFinderTitle.css("color", "blue");
+    $newRecipeFinderTitle.css("color", "yellow");
   }
 
 
   function newRecipeFinderSearchBlurred() {
-    /* $(document.body).css("overflow", "auto"); */
+    $(document.body).css("overflow", "auto");
 
     $navbar.show();
     $navbarDropdown.show();
@@ -37,7 +37,7 @@ $(document).ready(function() {
 
 /* ---------------------------- EVENT HANDLERS ---------------------------- */
   $newRecipeSearchBar.on("focus", function() {
-    /* document.ontouchmove = function(event) {
+    document.ontouchmove = function(event) {
       event.preventDefault();
       event.stopPropagation();
     }
@@ -46,23 +46,17 @@ $(document).ready(function() {
       setTimeout(function() {
         newRecipeFinderSearchFocused();
       }, 200);
-    } */
-
-    newRecipeFinderSearchFocused();
-
+    }
   });
 
   $newRecipeSearchBar.on("blur", function() {
-    /* document.ontouchmove = function(event) {
+    document.ontouchmove = function(event) {
       return true;
     }
 
     if(touchDevice === true) {
       newRecipeFinderSearchBlurred();
-    } */
-
-    newRecipeFinderSearchBlurred();
-
+    }
   });
 
 
