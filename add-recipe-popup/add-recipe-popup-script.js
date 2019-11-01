95780b71aa042a802f57575253631b40352cdb8d
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-11-01 10:37:19 -0400

    reset addRecipeFormGrid scroll position on submission/move clear all local storage testing to escape key press

--- add-recipe-popup/add-recipe-popup-script.js
+++ add-recipe-popup/add-recipe-popup-script.js
@@ -231,6 +231,7 @@ function submitAddRecipeForm(name, category, tags, ingredients, recipe) {
   getLocalStorage();
 
   $addRecipeForm[0].reset();
+  $addRecipeFormGrid.scrollTop( 0 );
 
   clearRecipeLists();
   clearTiles();
@@ -281,8 +282,8 @@ function getLocalStorage() {
 $(document).ready(function() {
 
   $(document).keydown(function(event) {
-  /* ----- 3 Num Key Press ----- */
-    if(event.which === 51) {
+  /* ----- Escape Key Press ----- */
+    if(event.which === 27) {
       localStorage.clear();
 
       console.log("local storage cleared");
